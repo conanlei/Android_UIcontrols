c73a4edc9a1496f7f987312914bcb64574a5c4b9
conanlei <514376839@qq.com>
2016-07-21 17:34:35 +0800

    first commit

---
+++ src/cn/eoe/uicontrols/AtyUsingScrollView.java
@@ -0,0 +1,14 @@
+package cn.eoe.uicontrols;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AtyUsingScrollView extends Activity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.aty_using_scrollview);
+	}
+}
